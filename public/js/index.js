53caca00bc86bafd223621353e68d976a3fc72ef
Ajit-tiwari <tajit40.at@gmail.com>
2020-05-09 17:58:56 +0530

    Add newMsg and createMsg

---
+++ public/js/index.js
@@ -0,0 +1,19 @@
+var socket = io();
+
+socket.on('connect', function () {
+    console.log('connected to server');
+
+    socket.emit('createMsg', {
+        text: 'fghgjhjcvjh',
+        from: 'Ajit'
+    });
+});
+
+socket.on('disconnect', function () {
+    console.log('disconnected to server');
+});
+
+//custom Event   ... on is used for listening event
+socket.on('newMessasge', function (msg) {    
+    console.log('New msg ',msg);
+})
--- server/server.js
+++ server/server.js
@@ -2,11 +2,35 @@ const path = require('path');
 var publicPath = path.join(__dirname,'../public');
 const port = process.env.PORT || 3000;
 const express = require('express');
-
+const socketIO = require('socket.io');
+const http = require('http');
 var app = express();
+var server = http.createServer(app);
+
+var io = socketIO(server);
+
+io.on('connection',(socket)=>{
+    console.log('New user connected');
+
+    socket.emit('newMessasge',{
+        from: 'Mike',
+        text: 'heyy',
+        createdAt: 56
+    });
+
+    socket.on('createMsg',(msg)=>{
+        console.log('createMsg',msg);
+    })
+
+    socket.on('disconnect', () => {
+        console.log('tab is closed');
+    });
+});
+
+
 
 app.use(express.static(publicPath));
 
-app.listen(port,()=>{
+server.listen(port,()=>{
     console.log(`Server is running at ${port} port`);
-})
+});
